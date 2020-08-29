f92177c7f565d7856d8cd3e8558d45aec2764cc8
Parth Chandna <pchandna24@gmail.com>
2020-08-29 22:38:11 +0530

    Update firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBUwbNuW9iNkp_Vwm6J1HP-_EaFWDwTsVs",
+  authDomain: "placement-test-aa445.firebaseapp.com",
+  databaseURL: "https://placement-test-aa445.firebaseio.com",
+  projectId: "placement-test-aa445",
+  storageBucket: "placement-test-aa445.appspot.com",
+  messagingSenderId: "549841430856",
+  appId: "1:549841430856:web:f5c40e297f7f136dfa6b7d",
+  measurementId: "G-TYLEYS18VZ",
+};
